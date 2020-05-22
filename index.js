7ac7854b886a73b11363453ffc5958a9e325150f
Daniel <ekklesia89@gmail.com>
2020-05-22 14:24:13 +0900

    graphql test

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    rollDice(numDice: Int!, numSides: Int): [Int],
+    test(str: String): String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  rollDice: function ({ numDice, numSides }) {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
+  },
+  test: ({ str }) => str.slice(1)
+};
+
+var app = express();
+app.use(express.json());
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
